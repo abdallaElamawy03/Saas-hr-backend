3832dd86cef0451244248a0240b74ae5846834b8
abdallaElamawy03 <abdallaelamawy03@gmail.com>
2026-01-11 01:44:52 +0200

    fix: correct allowed origin URL format

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,6 +1,6 @@
 const allowedOrigin = [
-  'saas-hr-frontend-3jl1n5c57-abdallahs-projects-c3ff95f0.vercel.app',
+  'https://saas-hr-frontend-3j11n5c57-abdallahs-projects-c3ff95f0.vercel.app',
   'http://localhost:3000',
-  
 ];
-module.exports=allowedOrigin
+
+module.exports = allowedOrigin;
