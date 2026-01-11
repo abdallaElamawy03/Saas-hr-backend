6c52d07e9f74939339ffb65a354d5f31568190b7
Abdalla Elamawy <abdallaelamawy03@gmail.com>
2026-01-11 02:08:11 +0200

    Clear allowedOrigin array for CORS configuration

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,6 +1,3 @@
-const allowedOrigin = [
-  'https://saas-hr-frontend-3j11n5c57-abdallahs-projects-c3ff95f0.vercel.app',
-  'http://localhost:3000',
-];
+const allowedOrigin = [];
 
 module.exports = allowedOrigin;
