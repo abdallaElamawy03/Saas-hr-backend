5bd35ea838fde7751caf3f374ce6383bd5a3e385
Abdalla Elamawy <abdallaelamawy03@gmail.com>
2026-01-11 02:05:15 +0200

    Remove trailing slash from localhost origin

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,6 +1,6 @@
 const allowedOrigin = [
   'https://saas-hr-frontend-3j11n5c57-abdallahs-projects-c3ff95f0.vercel.app',
-  'http://localhost:3000/',
+  'http://localhost:3000',
 ];
 
 module.exports = allowedOrigin;
